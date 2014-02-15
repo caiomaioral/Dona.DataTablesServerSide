b2f87d8afd110e01a0fe36c4ad93e4bb6c08bd4b
diedona <diedona@gmail.com>
2014-02-14 22:30:36 -0200

    Criação dos models + início do front-end

---
+++ Dona.DataTablesServerSide/Models/Emprego.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dona.DataTablesServerSide.Models
+{
+    public class Emprego
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Salario { get; set; }
+    }
+}
---
+++ Dona.DataTablesServerSide/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dona.DataTablesServerSide.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DtAniversario { get; set; }
+        public Emprego Emprego { get; set; }
+    }
+}
