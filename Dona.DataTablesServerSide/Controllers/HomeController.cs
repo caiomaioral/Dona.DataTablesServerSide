ba1687e035a667df1b8ce36e2cbfa3b0901cd232
diedona <diedona@gmail.com>
2014-02-15 12:37:53 -0200

    início do back-end

--- Dona.DataTablesServerSide/Controllers/HomeController.cs
+++ Dona.DataTablesServerSide/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dona.DataTablesServerSide.DAL;
+using Dona.DataTablesServerSide.ViewModel.DataTables;
+using Dona.DataTablesServerSide.ViewModel.Home;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,34 @@ namespace Dona.DataTablesServerSide.Controllers
 {
     public class HomeController : Controller
     {
+        private PessoaServico _servico = null;
+
+        public HomeController()
+        {
+            _servico = new PessoaServico();
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult BuscaAssincrona(JQueryDataTablesParamViewModel Params)
+        {
+            int TotalPessoas = 0;
+            int TotalPessoasFiltradas = 0;
+
+            List<ListarPessoaViewModel> Pessoas = _servico.PegarPessoas(Params, out TotalPessoas, out TotalPessoasFiltradas);
+
+            var Resultado = new
+            {
+                sEcho = Params.sEcho,
+                iTotalRecords = TotalPessoas,
+                iTotalDisplayRecords = TotalPessoasFiltradas,
+                aaData = Pessoas
+            };
+
+            return Json(Resultado, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Dona.DataTablesServerSide/DAL/PessoaServico.cs
@@ -0,0 +1,29 @@
+﻿using Dona.DataTablesServerSide.ViewModel.DataTables;
+using Dona.DataTablesServerSide.ViewModel.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dona.DataTablesServerSide.DAL
+{
+    public class PessoaServico
+    {
+        private StubDB _db = null;
+
+        public PessoaServico()
+        {
+            _db = new StubDB();
+        }
+
+        public List<ListarPessoaViewModel> PegarPessoas(JQueryDataTablesParamViewModel Params, out int TotalPessoas, out int TotalPessoasFiltradas)
+        {
+            List<ListarPessoaViewModel> Pessoas = new List<ListarPessoaViewModel>();
+
+            TotalPessoas = _db.Pessoas.Count();
+            TotalPessoasFiltradas = Pessoas.Count();
+
+            return Pessoas;
+        }
+    }
+}
