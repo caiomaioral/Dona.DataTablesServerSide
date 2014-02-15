b13472da33feb32ebd856aeeab0d5dff89e5c71e
diedona <diedona@gmail.com>
2014-02-15 12:02:46 -0200

    globalizando + view models

---
+++ Dona.DataTablesServerSide/ViewModel/Home/ListarPessoaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dona.DataTablesServerSide.ViewModel.Home
+{
+    public class ListarPessoaViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string DtAniversario { get; set; } //já em string pensando em exibir na grid via JSON
+        public int EmpregoId { get; set; }
+        public string EmpregoNome { get; set; }
+        public decimal EmpregoSalario { get; set; }
+
+    }
+}
